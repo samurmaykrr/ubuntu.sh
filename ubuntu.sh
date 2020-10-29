c5ea12291f5b41b5b8214b6b76a322b7f471d60c
Towha <57793061+Towha@users.noreply.github.com>
2020-10-29 16:28:07 +0600

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,97 +1,117 @@
 #!/usr/bin/env bash
 USER=$(whoami)
 export DEBIAN_FRONTEND=noninteractive
+export PATH="$HOME/.local/bin:$PATH"
+sudo rm -rf /var/lib/dpkg/lock
+sudo rm -rf /var/cache/debconf/*.*
 
-echo "This script is shit and bloated"
-sleep 5
+# colors
+NORMAL=`tput sgr0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+Done="${GREEN}Done ✓${NORMAL}"
+
 clear
+echo "${RED}Disclaimer:${NORMAL} This script is shit and bloated"
+sleep 5
 
-echo "Do you want to change server password?"
+echo "${RED}Do you want to change server password?${NORMAL}"
 read -p "y/n:
 " prompt
 if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
 then
   sudo passwd $USER
 else
-  echo "Password wasn't Changed."
+  echo "${GREEN}Password wasn't Changed.${NORMAL}"
 fi
 
 
+echo "${RED}Enabling Universe, Multiverse and Restricted repositories${NORMAL}"
+sleep 1
+sudo add-apt-repository universe > /dev/null
+sudo add-apt-repository multiverse > /dev/null
+sudo add-apt-repository restricted > /dev/null
+echo $Done
 
-# Enable multiverse repository
-sudo sed -i "/^# deb.*multiverse/ s/^# //" /etc/apt/sources.list
-sudo apt-get -y update
-sudo apt-get -y upgrade 
-sudo apt-get -y autoremove
+echo "${RED}Checking for updates.${NORMAL}"
+sleep 1
+sudo apt-get -y update > /dev/null
+sudo apt-get -y upgrade > /dev/null 2>&1
+sudo apt-get -y autoremove  > /dev/null
+echo $Done
+
+echo "${RED}Setting UTF8${NORMAL}"
+sleep 1
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
-	
-clear
-echo "adding a auto updater to crontab"
+sudo apt-get install -qq language-pack-en-base > /dev/null
+sudo apt-get install -qq software-properties-common > /dev/null
+echo $Done
+
+echo "${RED}Adding a auto updater to crontab${NORMAL}"
 sleep 1
-crontab -l > updater
-echo "0 0 * * *		 sudo apt-get update && sudo apt-get upgrade -y && sudo apt autoremove && echo updated@SUCCESS >> ~/.update.log" >> updater
-crontab updater
-rm updater
-clear
+sudo crontab -l > updater
+sudo echo "0 0 * * *    sudo apt-get update && sudo apt-get upgrade -y && sudo apt autoremove && echo updated@SUCCESS >> ~/.update.log" >> updater
+sudo crontab updater
+sudo rm updater
+echo $Done
 
-echo ################################################
-echo "Now we are gonna install all esential packages"
-sudo add-apt-repository -y ppa:apt-fast/stable 
-sudo apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y apt-fast && echo debconf apt-fast/maxdownloads string 16 | debconf-set-selections && echo debconf apt-fast/dlflag boolean true | debconf-set-selections && echo debconf apt-fast/aptmanager string apt-get | debconf-set-selections
+echo "${RED}Installing Apt-fast${NORMAL}"
+sudo add-apt-repository -y ppa:apt-fast/stable > /dev/null
+sudo apt-get -qq update > /dev/null && sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq apt-fast > /dev/null && sudo echo debconf apt-fast/maxdownloads string 16 | debconf-set-selections && sudo echo debconf apt-fast/dlflag boolean true | debconf-set-selections && sudo echo debconf apt-fast/aptmanager string apt-get | debconf-set-selections 
+echo $Done
 
-sudo apt-get -y install ncdu tree rar unrar zip unzip htop atop p7zip-full neovim vnstati
-clear && sleep 1
+echo "${RED}Installing day2day packages${NORMAL}"
+sudo apt-get install -qq ncdu tmux irssi tree rar unrar zip unzip htop atop p7zip-full neovim vnstati > /dev/null 2>&1
+echo $Done
 
-echo "Now installing python stuff"
-sudo apt-get -y install python3-pip python3-dev python3-utmp python3-virtualenv 
-sudo pip3 install virtualenvwrapper wheel gallery-dl youtube-dl requests bs4 lxml 
-clear && sleep 1
+echo "${RED}Now installing some python essential packages${NORMAL}"
+sudo apt-get install -qq python3-pip python3-dev python3-utmp python3-virtualenv > /dev/null 2>&1
+pip3 install virtualenvwrapper wheel gallery-dl youtube-dl requests bs4 lxml -q > /dev/null 2>&1
+echo $Done
 
-sudo apt-get -y install tmux irssi 
-clear && sleep 1
-
-echo "Installing rclone"
+echo "${RED}Installing rclone${NORMAL}"
 sleep 1
-curl https://rclone.org/install.sh | sudo bash
-clear && sleep 1
+curl -s https://rclone.org/install.sh | sudo bash > /dev/null 2>&1
+echo $Done
 
-echo "Installing vsftpd"
-sudo systemctl start vsftpd
-sudo systemctl enable vsftpd
+echo "${RED}Installing vsftpd${NORMAL}"
+sudo apt-get install -qq vsftpd  > /dev/null
+sudo systemctl start vsftpd  > /dev/null 2>&1
+sudo systemctl enable vsftpd > /dev/null 2>&1
 sudo tee -a /etc/vsftpduserlist.conf >> /dev/null <<'user'
 ubuntu
 towha
 root
 user
+sudo systemctl restart vsftpd  > /dev/null 2>&1
+echo $Done
 
-sudo systemctl restart vsftpd
-clear && sleep 1
+echo "${RED}Installing some compiling packages${NORMAL}"
+sudo apt-get install -qq build-essential libssl-dev autoconf automake cmake ccache libicu-dev git-core libass-dev zlib1g-dev yasm texinfo pkg-config libtool > /dev/null 2>&1
+echo $Done
 
-echo "Installing some compiling tools"
-sudo apt-get -y install build-essential libssl-dev autoconf automake cmake ccache libicu-dev git-core libass-dev zlib1g-dev yasm texinfo pkg-config libtool 
-clear && sleep 1
-
-echo "Installing ffmpeg, please refer to https://trac.ffmpeg.org/wiki/CompilationGuide/Ubuntu for extra codecs"
+echo "${RED}Installing ffmpeg, please refer to https://trac.ffmpeg.org/wiki/CompilationGuide/Ubuntu for extra codecs${NORMAL}"
 sleep 5
-sudo apt-get -y install ffmpeg 
-clear && sleep 1
+sudo apt-get install -qq ffmpeg > /dev/null 2>&1
+echo $Done
 
-echo "installing some more language packages"
-echo "skipping php due to reasons" 
-sudo apt-get -y install nginx golang docker.io perl default-jre apache2 && curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash - && sudo apt-get -y install nodejs 
-clear && sleep 1
+echo "${RED}Installing Language packages${NORMAL}"
+sudo add-apt-repository -y ppa:openjdk-r/ppa > /dev/null
+sudo add-apt-repository -y ppa:linuxuprising/libpng12 > /dev/null # I am skipping php due to reasons and only adding its repo in case there is a need to install it.
+sudo apt-get install -qq nginx golang docker.io perl openjdk-15-jre > /dev/null 2>&1 && curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash - > /dev/null && sudo apt-get -y install nodejs > /dev/null 
+echo $Done # sudo apt-get install -qq curl debconf-utils php-pear php7.4-curl php7.4-dev php7.4-gd php7.4-mbstring php7.4-zip php7.4-mysql php7.4-xml php7.4-fpm php7.4-intl php7.4-bcmath > /dev/null 
  
-echo "Installing aria2,rclone & transmission"
-sudo apt-get -y install aria2 
-sudo apt-get -y install transmission-cli transmission-daemon && sudo /etc/init.d/transmission-daemon stop && mkdir ~/downloads && sudo chown ubuntu:debian-transmission ~/downloads && sudo chmod g+w ~/downloads && clear && sed -i 's|"/var/lib/transmission-daemon/downloads"|"~/downloads"|g' /etc/transmission-daemon/settings.json && sed -i 's|"rpc-whitelist-enabled": true|"rpc-whitelist-enabled": false|g' /etc/transmission-daemon/settings.json && sed -i 's|"rpc-authentication-required": true|"rpc-authentication-required": false|g' /etc/transmission-daemon/settings.json 
+echo "${RED}Installing aria2 & transmission${NORMAL}"
+sudo apt-get install -qq aria2 > /dev/null
+sudo apt-get install -qq transmission-cli transmission-daemon > /dev/null && sudo /etc/init.d/transmission-daemon stop > /dev/null && mkdir ~/downloads && sudo chown ubuntu:debian-transmission ~/downloads && sudo chmod g+w ~/downloads && clear && sudo sed -i 's|"/var/lib/transmission-daemon/downloads"|"~/downloads"|g' /etc/transmission-daemon/settings.json && sudo sed -i 's|"rpc-whitelist-enabled": true|"rpc-whitelist-enabled": false|g' /etc/transmission-daemon/settings.json && sudo sed -i 's|"rpc-authentication-required": true|"rpc-authentication-required": false|g' /etc/transmission-daemon/settings.json > /dev/null
+echo $Done
 
-clear && sleep 1
 
-echo "changing MOTD" # remove this section before running code in case you don't need it
-sudo apt-get -y install update-motd
+echo "${RED}changing MOTD${NORMAL}" # "touch .hushlogin" to "remove" the motd instead of deleting it.
+sudo apt-get install -qq update-motd > /dev/null
 sudo rm -rf /etc/update-motd.d/*
-sudo apt-get -y install inxi screenfetch
+sudo apt-get install -qq inxi screenfetch > /dev/null
 sudo touch /etc/update-motd.d/01-custom 
 sudo chmod +x /etc/update-motd.d/01-custom
 
@@ -104,22 +124,22 @@ echo SYSTEM DISK USAGE
 export TERM=xterm; inxi -D
 echo
 MOTD
-clear && sleep 1
+echo $Done
 
-echo "Now installing oh-my-tmux"
-cd && git clone https://github.com/gpakosz/.tmux.git && ln -s -f .tmux/.tmux.conf && cp .tmux/.tmux.conf.local .
-clear && sleep 1
+echo "${RED}Now installing oh-my-tmux${NORMAL}"
+cd && git clone --quiet https://github.com/gpakosz/.tmux.git > /dev/null && ln -s -f .tmux/.tmux.conf > /dev/null && cp .tmux/.tmux.conf.local . 
+echo $Done
 
-echo "Now installing ZSH"
+echo "${RED}Now installing ZSH${NORMAL}"
 sleep 1
-sudo apt-get update && sudo apt-get install -y zsh && \
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-echo -e "Making Oh My Zsh hawt..."
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-    git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-    git clone https://github.com/zsh-users/zsh-completions $HOME/.oh-my-zsh/custom/plugins/zsh-completions
-	wget https://raw.githubusercontent.com/rupa/z/master/z.sh -O ~/.z
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
+sudo apt-get update -qq && sudo apt-get install -qq zsh > /dev/null 2>&1 && \
+git clone --quiet https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh > /dev/null  && cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc 
+echo -e "${GREEN}Making Oh My Zsh hawt...${NORMAL}"
+    git clone --quiet https://github.com/zsh-users/zsh-syntax-highlighting $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting > /dev/null 
+    git clone --quiet https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions > /dev/null 
+    git clone --quiet https://github.com/zsh-users/zsh-completions $HOME/.oh-my-zsh/custom/plugins/zsh-completions > /dev/null 
+	wget https://raw.githubusercontent.com/rupa/z/master/z.sh -q -O ~/.z > /dev/null 2>&1
+    git clone --quiet --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k > /dev/null
     [[ -z $(grep "autoload -U compinit && compinit" $HOME/.zshrc) ]] && echo "autoload -U compinit && compinit" >> $HOME/.zshrc
 	
     sed -i '/^ZSH_THEME=/c\ZSH_THEME="random"' $HOME/.zshrc
@@ -154,8 +174,7 @@ ALIAS
 
 sudo chsh -s /bin/zsh $USER
 sudo echo "bash -c zsh" >> .bashrc # This is used since for some cloud service changing the shell isn't permitted so a work around for it.
-clear && sleep 1
+echo $Done
 
-echo "ALL DONE!"
-echo "system will now reboot!"
-sudo reboot
+echo "${GREEN}ALL DONE!${NORMAL}"
+echo "${GREEN}It is recommended to ${RED}reboot${NORMAL} your server now!${NORMAL}"
